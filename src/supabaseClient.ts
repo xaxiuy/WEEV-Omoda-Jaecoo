91acb3b96a817ebeb982e687ea9021333d3348f5
Rodrigo Gonzalez <tu@email.com>
2026-01-15 15:42:45 -0300

    Add Supabase credentials to supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-// TODO: Inserta tu ANON KEY aquí
 const supabaseUrl = 'https://yjyptkurssqlxiguqlfb.supabase.co';
-const supabaseAnonKey = 'TU_ANON_KEY_AQUI';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlqeXB0a3Vyc3NxbHhpZ3VxbGZiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0OTQ2MjUsImV4cCI6MjA4NDA3MDYyNX0.yeX6y9uhg35BsAQ2jY7GHAiKUJILQFRIqRlo0pf3sYc';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
